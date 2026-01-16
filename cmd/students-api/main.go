fc4e7b3152175da78a7d6eb8c63231063a69b4c3
Sky471012 <sharma.aakash1012@gmail.com>
2026-01-16 23:32:21 +0530

    Progress made

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,6 +1,61 @@
 package main
-import "fmt"
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Sky471012/go-project/internal/http/handlers/student"
+	"github.com/Sky471012/go-project/internal/config"
+)
 
 func main() {
-	fmt.Println("Hi")
+	//load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /api/students", student.New())
+	
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+
 }
